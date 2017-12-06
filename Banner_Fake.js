ae8cd0bc1a669411bbbb72a7c1031d98d7c238d1
Don Drury <don@agruity.com>
2017-12-06 13:53:56 -0600

    build for frankly

---
+++ Banner_Fake.js
@@ -0,0 +1,45 @@
+var banners = [
+
+  {
+    'Title': 'Tonight At 10',
+    'Type': 'link',
+    'Link': 'http://www.newson6.com/story/18251082/tonight-at-10',
+    'Description': 'Big reveal of that story we\'ve been talking about',
+    'Target': '_self',
+    'BannerTypeId': 3,
+    'BannerInfoTypeMask': 7,
+    'EncoderTypeId': 0,
+    'Audio': false,
+    'Theme': '',
+    'Content': 'Livestreaming'
+  },
+  {
+    'Title': 'Earthquake Alert',
+    'Type': 'earthquake',
+    'Link': 'http://www.newson6.com/Earthquake',
+    'Description': 'At 7:59AM a magnitude 1.3 was reported in Craig County',
+    'Target': '_self',
+    'BannerTypeId': 15,
+    'BannerInfoTypeMask': 71,
+    'EncoderTypeId': 0,
+    'Audio': false,
+    'Theme': '',
+    'Content': ''
+  },
+  {
+    'Title': 'Breaking News',
+    'Type': 'earthquake',
+    'Link': 'http://www.newson6.com/space-aliens',
+    'Description': 'Police Officer Accused of Not Shooting unarmed man',
+    'Target': '_self',
+    'BannerTypeId': 0,
+    'BannerInfoTypeMask': 71,
+    'EncoderTypeId': 0,
+    'Audio': false,
+    'Theme': '',
+    'Content': ''
+  }
+
+];
+
+export default banners;
