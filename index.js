68a567ee5f02a58fb57dc0bd77327f4919d64d8c
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-12-08 13:37:30 -0500

    printing TempNav

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ class Header extends Component {
   render() {
     return (
       <div className='gnm-header'>
-      <h1>sup</h1>
+      <pre>{JSON.stringify(TempNav};</pre>
         <Banner affiliate={this.affiliate} />
         <div id='gnm-header-without-banner'>
           <div className='container'>
